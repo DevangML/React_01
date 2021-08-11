b7da65d7a7e80f8c5659329dc3401ade9eddc045
Devang <devangmanjramkar@gmail.com>
2021-08-11 14:30:36 +0530

    First-Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from '../src/Data.json'
+import { useState } from 'react'
 
 function App() {
+  const [search, setsearch] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchc">
+        <input type="text" placeholder="Search..." onChange={event => { setsearch(event.target.value) }} />
+        <br />
+      </div>
+      <div className="crd-cont">
+      {Data.filter((val) => {
+        if (search == "") {
+          return (
+            <div className="wrapper">
+              <Card
+                img="https://source.unsplash.com/random"
+                title={val.name}
+                description={val.website}
+              />
+            </div>
+          );
+        }
+        else if (val.name.toLowerCase().includes(search.toLowerCase())) {
+          return val
+        }
+      }).map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <div className="card">
+              <img className="crd" src="https://source.unsplash.com/random" />
+              <p>{val.name}</p>
+              <p>{val.email}</p>
+            </div>
+          </div>
+        );
+      })}
+      </div>
+    </div>
+
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src={props.img} class="card__image" />
+        <h2 className="card__title">{props.title}</h2>
+        <p className="card__description">{props.description}</p>
+      </div>
+      <button className="card__btn">View Recipe</button>
     </div>
   );
 }
